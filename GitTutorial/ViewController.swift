db8037038e7d35909c1ab0d38914cd9b0e3f14e3
Adi <mincaadrian2021@yahoo.com>
2019-01-08 18:53:27 +0200

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Adi on 08/01/2019.
+//  Copyright © 2019 Adrian-Minca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        helloWorld()
+        
+    }
+
+    
+    func helloWorld(){
+        
+        print("Yes I Made it!")
+        
+    }
+
+}
+
